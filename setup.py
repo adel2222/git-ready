6eb942239482dff21e70c396238ea21873799d3d
Dolph Mathews <dolph.mathews@gmail.com>
2016-03-11 05:13:22 +0000

    Minor version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import setuptools
 
 setuptools.setup(
     name='git-ready',
-    version='1.0.1',
+    version='1.0.2',
     description='Get ready to commit, socially.',
     author='Dolph Mathews',
     author_email='dolph.mathews@gmail.com',
