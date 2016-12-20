925af98bead850056d5ff3a33dc5ab5a366e7fa7
Dolph Mathews <dolph.mathews@gmail.com>
2016-12-20 10:15:59 -0600

    Use cwd

--- test.sh
+++ test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 set -e
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+# Testing dependencies.
 apt-get update
 apt-get install git python-pip -y
-pip install -e .
+
+pip install -e $SCRIPTPATH
 git clone --depth=1 https://github.com/openstack/nova.git
 cd nova
 
