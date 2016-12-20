5f7e0a4661864adb82c6d65252d020fc45999701
Dolph Mathews <dolph.mathews@gmail.com>
2016-12-20 10:07:39 -0600

    Add basic script to smoke test

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+apt-get install git python-pip -y
+pip install -e .
+git clone --depth=1 https://github.com/openstack/nova.git
+cd nova
+
+# Test with no branch specified.
+git ready
+
+# Test with a particular branch specified.
+git ready master
