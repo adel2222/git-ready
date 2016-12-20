57c84cd85850796a164a7f548397403d7c97d276
Dolph Mathews <dolph.mathews@gmail.com>
2016-12-20 12:34:53 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import setuptools
 
 setuptools.setup(
     name='git-ready',
-    version='1.1.0',
+    version='1.1.1',
     description='Get ready to commit, socially.',
     author='Dolph Mathews',
     author_email='dolph.mathews@gmail.com',
